2f089531ed06c65c163a735b8b2228a36c54433a
Bimsara Gunarathna <44497039+BimsaraGunarathna@users.noreply.github.com>
2020-04-28 05:32:19 +0530

    unnecessary package removed

--- src/Api.js
+++ src/Api.js
@@ -3,12 +3,7 @@ import { sampleProducts } from "./Data";
 //axios
 import axios from 'axios';
 
-import { useAlert } from "react-alert";
 
-///
-//
-// Methods of this class are used to simulate calls to server.
-//
 class Api {
 
   //add a product rating by user
